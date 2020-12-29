ed871330670772137b2a848e4f8dd1a06af98c00
kim-dev <lunatic7208@gmail.com>
2020-12-30 03:08:35 +0900

    마테리얼추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component   } from 'react'; //useState  hook(함수형컴포넌트에서 할수없었던 다양한 작업을 해주는 것) 호출 
+                                                        //useState 함수영 컴포넌트일떄 가변적인 상태를지닐수있게 해주는 상태관리
+import './App.scss';
+import Main from './main/main'
+import { Container, AppBar, Toolbar, IconButton, Typography, Button, withStyles } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+//props ===@input
+//sate 
+//defaultProps props 정의 안할떄
+//클래스형이 과거 함수형이 최근
+//클래스형은 라이플 사이클로 제어
+//함수형은 hook
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+const useStyles = (theme: { spacing: (arg0: number) => any; }) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+});
+class App extends Component {
+
+  // 라이플사이클
+  // constructor
+  // render
+  // componentDidMount
+  
+  constructor(props: {} | Readonly<{}>) {
+    console.log('constructor');
+    super(props);
+  }
+  componentDidUpdate(){
+  }
+  componentDidMount() {
+    //afterviewinit?
+  
+    console.log('componentDidMount');
+  }
+  public render() {
+    console.log(this.props) //컴포넌트 재활용?
+    const { classes }:any = this.props;
+    return <div className="wrap">
+      <header>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
       </header>
+      <Container maxWidth="sm">
+        <Main></Main>
+      </Container>
+      <footer>
+
+      </footer>
     </div>
-  );
+  }
+
 }
 
-export default App;
+export default withStyles(useStyles)(App); 
+//withStyles hoc :  HOC란 컴포넌트를 인자로 받아 새로운 컴포넌트를 반환하는 디자인 패턴으로 작성한 컴포넌트를 말한다. 인자로 받은 컴포넌트의 props를 그대로 유지하고 새로운 값을 추가해준다는 특징
+
---
+++ src/main/main.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './main.scss';
+
+class Main extends Component{
+  public render(){
+    return <span>test</span>;
+  }
+}
+
+export default Main;
